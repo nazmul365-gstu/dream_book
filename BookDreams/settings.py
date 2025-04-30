efa121d52435297c5ec3fe4976274936aab6331e
alam0164088 <168212623+alam0164088@users.noreply.github.com>
2025-04-30 07:53:26 +0600

    Added allowed host for render domain

--- BookDreams/settings.py
+++ BookDreams/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-CHANGE_THIS_TO_A_SECURE_KEY'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['book-dreams.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
